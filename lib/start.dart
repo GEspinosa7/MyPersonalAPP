dd38afec2d8f034a67cee28fbb0336809f99378a
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-05-05 14:57:59 -0300

    add temporary logo in start

--- lib/start.dart
+++ lib/start.dart
@@ -14,12 +14,30 @@ Widget build(BuildContext context) {
 				        centerTitle: true,
 				        backgroundColor: Color.fromARGB(255, 18, 32, 47),
 		      	),
-		      	body: Center (
-                child: Column (
-
-                  mainAxisAlignment: MainAxisAlignment.center,
-		      		children: [
-      		            FlatButton(
+		      	body: 
+            Padding(
+              padding: EdgeInsets.all(60),
+              child:  Center (
+                child: 
+                  Column (
+mainAxisAlignment: MainAxisAlignment.spaceAround,
+crossAxisAlignment: CrossAxisAlignment.stretch,
+		      		      children: [
+                      Center(
+                        child:
+                        Container(
+                          height: 100,
+                          width: 100,
+                          child: Image.asset('assets/img/logo.png')
+                        )
+                      ),
+                      Column(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                        ButtonTheme(
+      		           height: 50,
+                     child :FlatButton(
                         child: Text("Login",style: TextStyle(fontSize: 20.0),),
                         color: Colors.blue,
                         textColor: Colors.white,
@@ -27,19 +45,25 @@ Widget build(BuildContext context) {
                         onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) {
                           return LoginPage();
                         }))
-                      ),
-
-      		            FlatButton(
-                      child: Text("Criar Conta",style: TextStyle(fontSize: 20.0),),
+                      )
+                ),
+                      Divider(),
+                ButtonTheme(
+                  height: 50,
+      		            child: FlatButton(
+                      child: Text("Nova Conta",style: TextStyle(fontSize: 20.0),),
       						    color: Colors.green,
               				textColor: Colors.white,
               				padding: EdgeInsets.all(10.0),
                         onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) {
                           return CreateAcPage();
                         }))
-        				),
-		      		]
+        				    )
+                ),
+              ])]
 				)
-           ));
+           )
+            )
+             );
 		}
 	}
