94ec5b943cbfcef57ae6aee4bac0a77067639b73
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-05-05 14:56:55 -0300

    Add list of trainers with Fake API json

--- lib/searchTrainer.dart
+++ lib/searchTrainer.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-import 'models/trainers.dart';
 
 final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
-
-// API FAKE
-const baseUrl = 'http://www.mocky.io';
 
 class ListTrainer  extends StatefulWidget {
   @override
@@ -13,87 +9,103 @@ class ListTrainer  extends StatefulWidget {
 }
 
 class _ListTrainerState extends State<ListTrainer> {
-  List<Trainers> trainers;
 
-    final client = Dio(BaseOptions(baseUrl: baseUrl));
-
-    _getTrainer() async {
-      var resp = await client.get('/v2/5eb02a103300002b00c68c3a');
-
-      setState(() {
-        trainers = List<Trainers>.from(resp.data.map((r) => Trainers.fromJson(r)));
-      });
-    }
+    //CONSULTAR API FAKE
+    var trainers = [];
 
     @override 
     void initState(){
       super.initState();
-      _getTrainer();
+      
+      Dio().get('http://www.mocky.io/v2/5eb19f5c3200000f4528f891').then((resp) {
+        
+        setState(() {
+          trainers = resp.data;
+        });
+    
+      });
+
     }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text('Personais Cadastrados', textAlign: TextAlign.center),
           centerTitle: true,
           backgroundColor: Colors.transparent,
         ),
-      body: _trainers()
-    )
+        body: trainerList()
     );
   }
 
-  Widget _trainers() {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView(children: List<Widget>.from(trainers.map((r) => listItem(r)).toList())),
-        )
-      ]
+  Widget trainerList(){
+    return Center (
+      child: 
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children : trainers.map((trainer){
+              return Column (
+                        children: [
+                          Container (
+                          margin: EdgeInsets.all(20),
+                          height: 150,
+                          width: double.infinity,
+                          decoration:
+                            BoxDecoration(
+                              border: Border.all(color: Colors.green),
+                              borderRadius: const BorderRadius.all(const Radius.circular(10)),
+                            ),  
+                            child:
+                            Padding(
+                            padding: EdgeInsets.only(top: 5, bottom: 5),
+                            child:
+                              Row (
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                Column (
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Container(
+                                      decoration:
+                                        BoxDecoration(
+                                        border: Border.all(color: Colors.green),
+                                        borderRadius: const BorderRadius.all(const Radius.circular(100)),
+                                      ), 
+                                    child:
+                                      Center (
+                                        child:
+                                        ClipRRect(
+                                          borderRadius: const BorderRadius.all(const Radius.circular(100)),
+                                          child:
+                                            Image.network(trainer['profile_picture'], width: 70, height: 70))
+                                      )
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(trainer['firstname'] + ' '),
+                                        Divider(),
+                                        Text(trainer['lastname']),
+                                      ],
+                                    )
+                                  ]
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children:[
+                                    Text('Avaliação'),
+                                    Divider(),
+                                    Text('${trainer['avaliation']}/5.0'),
+                                  ]
+                                )
+                                ]
+                              ),
+                            )
+                          )               
+                        ],
+                      );
+            }).toList()
+          )
     );
-  }
-
-  listItem(Trainers trainers) {
-    return Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-                border: Border.all(color: Colors.green),
-                borderRadius:
-                    const BorderRadius.all(const Radius.circular(10)),
-              ),
-        height: 120,
-        width: double.infinity,
-        child: Padding(
-          padding: EdgeInsets.all(15.0),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Column(children: [
-              Container(
-                margin: EdgeInsets.only(top: 10, bottom:10),
-                decoration: BoxDecoration(
-                border: Border.all(width: 25, color: Colors.black38),
-                borderRadius:
-                    const BorderRadius.all(const Radius.circular(100)),
-              )),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text (trainers.firstname),
-                    Text (trainers.lastname)
-                  ],
-                )
-              )
-            ]),
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Text('Avaliação'), Text('${trainers.avaliation}/5.0')]),
-          ]),
-        )
-        );
   }
 }
