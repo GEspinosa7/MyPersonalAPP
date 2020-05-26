2fe2653ae2f990e051d307dcb8dff7c1726e8985
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-05-26 12:37:03 -0300

    add all screens for my profile  area

---
+++ lib/change_email.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+class  ChangeEmail extends StatefulWidget {
+  @override
+  _ChangeEmailState createState() => _ChangeEmailState();
+}
+
+class _ChangeEmailState extends State<ChangeEmail> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+            theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(
+                title: Text('Alterar E-mail', textAlign: TextAlign.center),
+                centerTitle: true,
+                backgroundColor: Colors.transparent,
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              ),
+              body: Text('foi')
+            )
+    );
+  }
+}
