8a4d73253aa453ffe2a85b16db9eb3e373321fbf
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-07-01 10:29:23 -0300

    update pages

--- Clients/lib/pages/login.dart
+++ Clients/lib/pages/login.dart
@@ -19,94 +19,93 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: mainGreen,
-        body: Container(
-          padding: const EdgeInsets.only(right: 30, left: 30),
-          child: Form(
-          key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(bottom: 50),
-                  child: Image.asset('assets/img/logo_texto.png'),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  child: Center(
-                    child: Text('Entre com usa conta', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
-                  ),
-                ),
-                TextFormField(
-                  autofocus: true,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration:  InputDecoration(
-                    hintText: 'Informe seu email', 
-                    hintStyle: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  onSaved: (value) => _email = value
-                ),
-                TextFormField(
-                  autofocus: true,
-                  obscureText: true,
-                  decoration:  InputDecoration(
-                    hintText: 'Informe sua senha', 
-                    hintStyle: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  onSaved: (value) => _password = value,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        FlatButton(
-                          color: Colors.transparent,
-                          textColor: Colors.white,
-                          padding: EdgeInsets.all(10.0),
-                          onPressed: _gotoCreate, child: Text(
-                            "Não tenho uma conta",
-                            style: TextStyle(
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.bold
-                            )
-                          )
+        backgroundColor: mainGreen,
+        body: SingleChildScrollView(
+          child: Container(
+              padding: const EdgeInsets.only(right: 30, left: 30),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(bottom: 50),
+                      child: Image.asset('assets/img/logo_texto.png'),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(bottom: 20),
+                      child: Center(
+                        child: Text(
+                          'Entre com usa conta',
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.bold),
                         ),
-                        RaisedButton(
-                          color: mainBlack,
-                          textColor: Colors.white,
-                          padding: EdgeInsets.all(10.0),
-                          onPressed: _loging, child: Text("Entrar", style: TextStyle(fontSize: 20.0))
+                      ),
+                    ),
+                    TextFormField(
+                        autofocus: true,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          hintText: 'Informe seu email',
+                          hintStyle: TextStyle(fontWeight: FontWeight.bold),
                         ),
-                      ],
-                    )
-
-                  ),
-                ],
-              ),
-          )
-        )
-    );
+                        onSaved: (value) => _email = value),
+                    TextFormField(
+                      autofocus: true,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Informe sua senha',
+                        hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onSaved: (value) => _password = value,
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            FlatButton(
+                                color: Colors.transparent,
+                                textColor: Colors.white,
+                                padding: EdgeInsets.all(10.0),
+                                onPressed: _gotoCreate,
+                                child: Text("Não tenho uma conta",
+                                    style: TextStyle(
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold))),
+                            RaisedButton(
+                                color: mainBlack,
+                                textColor: Colors.white,
+                                padding: EdgeInsets.all(10.0),
+                                onPressed: _loging,
+                                child: Text("Entrar",
+                                    style: TextStyle(fontSize: 20.0))),
+                          ],
+                        )),
+                  ],
+                ),
+              )),
+        ));
   }
 
   _gotoCreate() async {
-     Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => CreatePage()));
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (context) => CreatePage()));
   }
 
   _loging() async {
     _formKey.currentState.save();
 
     try {
-      await _auth.signInWithEmailAndPassword(email: _email, password: _password);
+      await _auth.signInWithEmailAndPassword(
+          email: _email, password: _password);
       Navigator.of(context)
-        .pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+          .pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
     } catch (e) {
       print(e);
     }
   }
-
-
 }
 
-class Register{}
+class Register {}
