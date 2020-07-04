372f006c87f23e5cf882dfdc7a1e29859e37bbc1
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-07-04 12:57:23 -0300

    add ratings to app

---
+++ Clients/lib/services/rating_service.dart
@@ -0,0 +1,24 @@
+import 'package:MyPersonal/models/rating_model.dart';
+import 'package:MyPersonal/utils/http.dart';
+import 'package:dio/dio.dart';
+
+class RatingService {
+  final _dio = Http.instance;
+
+  Future<RatingModel> createRating(RatingModel rating, String uid) async {
+    final resp = await _dio.post('/ratings',
+        data: rating.toJson(), options: Options(headers: {'token': uid}));
+
+    return RatingModel.fromJson(resp.data);
+  }
+
+  Future<RatingModel> getRating(String uid) async {
+    final resp =
+        await _dio.get('/ratings', options: Options(headers: {'token': uid}));
+
+    if (resp.data.length > 0)
+      return RatingModel.fromJson(resp.data[0]);
+    else
+      return null;
+  }
+}
